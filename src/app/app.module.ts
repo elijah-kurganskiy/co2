a043ca8248d67c780d867980ae4e3b0b604ec326
Ilia Kurganskiy <kurganskiy@mercurydevelopment.com>
2021-02-26 12:53:50 +0400

    build: add components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxsModule } from '@ngxs/store';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { MatTableModule } from "@angular/material/table";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsModule } from "@ngxs/store";
+import { Co2DialogComponent } from "../features/modal/co2-dialog/co2-dialog.component";
+import { Co2TableComponent } from "../features/table/co2-table/co2-table.component";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    NgxsModule.forRoot([])
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    MatTableModule,
   ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent, Co2TableComponent, Co2DialogComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
