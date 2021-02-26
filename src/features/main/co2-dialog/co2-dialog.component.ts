c51488318745acb6bd3ea68b60a6f7615823a959
Ilia Kurganskiy <kurganskiy@mercurydevelopment.com>
2021-02-26 17:25:40 +0400

    change name for dialog

--- src/features/main/co2-dialog/co2-dialog.component.ts
+++ src/features/main/co2-dialog/co2-dialog.component.ts
@@ -6,7 +6,7 @@ import {
   MatDialogRef,
 } from "@angular/material/dialog";
 import { ActivatedRoute, Router } from "@angular/router";
-import { map, switchMap } from "rxjs/operators";
+import { filter, map, switchMap } from "rxjs/operators";
 import { sectors } from "../../../store/co2-store/const/sector.const";
 import { Co2Model } from "../../../store/co2-store/models/co2.model";
 import { Co2ModalDialogComponent } from "../co2-modal-dialog/co2-modal-dialog.component";
@@ -23,10 +23,10 @@ import {
 })
 export class Co2DialogComponent implements OnInit {
   public sectors = sectors;
-  sector = new FormControl("", [Validators.required]);
+  public isEditMode = false;
 
   co2Form = new FormGroup({
-    sector: this.sector,
+    sector: new FormControl("", [Validators.required]),
     emoji: new FormControl(":)", [Validators.required]),
     value: new FormControl(0, [
       Validators.required,
@@ -44,6 +44,7 @@ export class Co2DialogComponent implements OnInit {
   ) {}
 
   public ngOnInit() {
+    this.isEditMode = !!this.data.item?.id;
     this.co2Form.reset({
       value: this.data.item?.co2Value ?? 0,
       sector: this.data.item?.sector ?? "",
@@ -61,6 +62,7 @@ export class Co2DialogComponent implements OnInit {
         data: this.co2Form.value.emoji,
       })
       .afterClosed()
+      .pipe(filter((result) => !!result))
       .subscribe((result) =>
         this.co2Form.patchValue({
           emoji: result,
