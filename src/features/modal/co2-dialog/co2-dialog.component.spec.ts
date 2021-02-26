5a1525c0ae0fa4e78f144536e7adde798c63f046
Ilia Kurganskiy <kurganskiy@mercurydevelopment.com>
2021-02-26 13:01:15 +0400

    build: migrate to angular 11

--- src/features/modal/co2-dialog/co2-dialog.component.spec.ts
+++ src/features/modal/co2-dialog/co2-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { Co2DialogComponent } from './co2-dialog.component';
 
@@ -6,7 +6,7 @@ describe('Co2DialogComponent', () => {
   let component: Co2DialogComponent;
   let fixture: ComponentFixture<Co2DialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ Co2DialogComponent ]
     })
--- src/features/table/co2-table/co2-table.component.spec.ts
+++ src/features/table/co2-table/co2-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { Co2TableComponent } from './co2-table.component';
 
@@ -6,7 +6,7 @@ describe('Co2TableComponent', () => {
   let component: Co2TableComponent;
   let fixture: ComponentFixture<Co2TableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ Co2TableComponent ]
     })
